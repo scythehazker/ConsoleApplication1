e8baee811f34d6683b7e261940ce2fe5713618ff
Влад Военков <voenkovv@mail.ru>
2021-04-11 14:47:53 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#pragma optimize(3)
+#pragma optimize("Ofast")
+#pragma target("avx,avx2,sse,sse2,sse3,ssse3,sse4,sse4.1,sse4.2,mmx,abm")
+#pragma optimize("Ofast")
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+using ll = long long;
+using pii = pair <int, int>;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	
+}
